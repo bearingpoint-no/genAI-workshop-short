582a4b8173bca1238915e0336e3df9bd2c8ee421
Arne Rustad <arne.rustad@bearingpoint.com>
2024-09-03 16:45:37 +0200

    Created fetch function for the data

--- hackathon/paths.py
+++ hackathon/paths.py
@@ -7,3 +7,4 @@ EVALUATION_DIR = os.path.join(_PACKAGE_DIR, "evaluation")
 
 REQUIREMENTS_COLAB_PATH = os.path.join(_REPOSITORY_DIR, "requirements_colab.txt")  # noqa E501
 DOTENV_PATH = os.path.join(_REPOSITORY_DIR, ".env")
+PRODUCT_REVIEWS_PATH = os.path.join(_PACKAGE_DIR, "data", "product_reviews.xlsx")  # noqa E501
